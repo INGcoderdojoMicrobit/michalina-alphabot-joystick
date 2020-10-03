837713905ef67f5193aa07c59d0df99ff9c3f978
INGcoderdojoMicrobit <71919260+INGcoderdojoMicrobit@users.noreply.github.com>
2020-10-03 10:59:34 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 WSJoyStick.JoyStickInit()
+radio.setGroup(123)
 basic.showIcon(IconNames.Asleep)
 basic.forever(function () {
     if (WSJoyStick.Listen_Key(KEY.E)) {
@@ -33,5 +34,13 @@ basic.forever(function () {
             # . . . .
             # . . . .
             `)
+    } else if (WSJoyStick.Listen_Dir(DIR.U)) {
+        images.arrowImage(ArrowNames.North).showImage(0)
+    } else if (WSJoyStick.Listen_Dir(DIR.D)) {
+        images.arrowImage(ArrowNames.South).showImage(0)
+    } else if (WSJoyStick.Listen_Dir(DIR.L)) {
+        images.arrowImage(ArrowNames.West).showImage(0)
+    } else if (WSJoyStick.Listen_Dir(DIR.R)) {
+        images.arrowImage(ArrowNames.East).showImage(0)
     }
 })
