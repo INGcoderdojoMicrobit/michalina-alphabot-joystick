46cfa0fa6ecfd4bd301c34e7a796f50e42f98083
INGcoderdojoMicrobit <71919260+INGcoderdojoMicrobit@users.noreply.github.com>
2020-10-03 10:42:44 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,12 +2,36 @@ WSJoyStick.JoyStickInit()
 basic.showIcon(IconNames.Asleep)
 basic.forever(function () {
     if (WSJoyStick.Listen_Key(KEY.E)) {
-        images.arrowImage(ArrowNames.North).showImage(0)
+        basic.showLeds(`
+            # # # # .
+            # . . . .
+            # # # . .
+            # . . . .
+            # # # # .
+            `)
     } else if (WSJoyStick.Listen_Key(KEY.C)) {
-        images.arrowImage(ArrowNames.South).showImage(0)
+        basic.showLeds(`
+            # # # # .
+            # . . . .
+            # . . . .
+            # . . . .
+            # # # # .
+            `)
     } else if (WSJoyStick.Listen_Key(KEY.D)) {
-        images.arrowImage(ArrowNames.East).showImage(0)
-    } else if (WSJoyStick.Listen_Key(KEY.E)) {
-        images.arrowImage(ArrowNames.West).showImage(0)
+        basic.showLeds(`
+            # # # . .
+            # . . # .
+            # . . # .
+            # . . # .
+            # # # . .
+            `)
+    } else if (WSJoyStick.Listen_Key(KEY.F)) {
+        basic.showLeds(`
+            # # # # .
+            # . . . .
+            # # # . .
+            # . . . .
+            # . . . .
+            `)
     }
 })
