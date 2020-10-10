e7c9d844855afef39044b8602e44a28e3e8f506b
INGcoderdojoMicrobit <71919260+INGcoderdojoMicrobit@users.noreply.github.com>
2020-10-10 10:22:01 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,51 +1,13 @@
+let Y = 0
+let V = 0
 WSJoyStick.JoyStickInit()
 radio.setGroup(123)
 radio.sendString("S,W,N,E")
 basic.showIcon(IconNames.Asleep)
 basic.forever(function () {
-    if (WSJoyStick.Listen_Key(KEY.E)) {
-        basic.showLeds(`
-            # # # # .
-            # . . . .
-            # # # . .
-            # . . . .
-            # # # # .
-            `)
-        radio.sendString("E")
-    } else if (WSJoyStick.Listen_Key(KEY.C)) {
-        basic.showLeds(`
-            . # # # .
-            # . . . .
-            # . . . .
-            # . . . .
-            . # # # .
-            `)
-        radio.sendString("C")
-    } else if (WSJoyStick.Listen_Key(KEY.D)) {
-        basic.showLeds(`
-            # # # . .
-            # . . # .
-            # . . # .
-            # . . # .
-            # # # . .
-            `)
-        radio.sendString("D")
-    } else if (WSJoyStick.Listen_Key(KEY.F)) {
-        basic.showLeds(`
-            # # # # .
-            # . . . .
-            # # # . .
-            # . . . .
-            # . . . .
-            `)
-        radio.sendString("F")
-    } else if (WSJoyStick.Listen_Dir(DIR.U)) {
-        images.arrowImage(ArrowNames.North).showImage(0)
-    } else if (WSJoyStick.Listen_Dir(DIR.D)) {
-        images.arrowImage(ArrowNames.South).showImage(0)
-    } else if (WSJoyStick.Listen_Dir(DIR.L)) {
-        images.arrowImage(ArrowNames.West).showImage(0)
-    } else if (WSJoyStick.Listen_Dir(DIR.R)) {
-        images.arrowImage(ArrowNames.East).showImage(0)
-    }
+    serial.writeValue("y", V)
+    Y = pins.analogReadPin(AnalogPin.P2)
+    V = Math.round(Math.map(Y, 0, 1023, -255, 255))
+    radio.sendNumber(V)
+    basic.pause(10)
 })
