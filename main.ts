18ddb2d0bc9cfb86edf9a48742c98f60d5852eb2
INGcoderdojoMicrobit <71919260+INGcoderdojoMicrobit@users.noreply.github.com>
2020-10-10 10:44:11 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+let v2 = 0
+let x = 0
 let Y = 0
 let V = 0
 WSJoyStick.JoyStickInit()
@@ -7,7 +9,10 @@ basic.showIcon(IconNames.Asleep)
 basic.forever(function () {
     serial.writeValue("y", V)
     Y = pins.analogReadPin(AnalogPin.P2)
-    V = Math.round(Math.map(Y, 0, 1023, -255, 255))
-    radio.sendNumber(V)
+    V = Math.round(Math.map(Y, 0, 1023, -100, 100))
+    x = pins.analogReadPin(AnalogPin.P1)
+    v2 = Math.round(Math.map(x, 0, 1023, -100, 100))
+    radio.sendValue("v", V)
+    radio.sendValue("v2", v2)
     basic.pause(10)
 })
